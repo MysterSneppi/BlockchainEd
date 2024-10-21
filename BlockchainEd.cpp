2fe55e5adf0145d0bc9b301abe3b14357f8340e1
MysterSneppi <aleksejk777a@gmail.com>
2024-10-21 16:51:48 +0300

    Add Blockchain class

--- BlockchainEd.cpp
+++ BlockchainEd.cpp
@@ -2,44 +2,76 @@
 #include <string>
 #include <sstream>
 #include <openssl/sha.h>
+#include <vector>
+#include <iomanip>
+
 #pragma comment(lib, "libcrypto.lib")
 #pragma comment(lib, "libssl.lib")
+
+using namespace std;
+
 
 class Block {
 public:
     int index;
-    std::string data;
-    std::string prevHash;
-    std::string hash;
+    string data;
+    string prevHash;
+    string hash;
 
-    Block(int idx, const std::string& data, const std::string& prevHash)
+    Block(int idx, const string& data, const string& prevHash)
         : index(idx), data(data), prevHash(prevHash), hash(calculateHash()) {
     }
-    
+
 private:
-    std::string calculateHash() const {
-        std::stringstream ss;
+    string calculateHash() const {
+        stringstream ss;
         ss << index << data << prevHash;
 
-        std::string input = ss.str();
+        string input = ss.str();
         unsigned char hash[SHA256_DIGEST_LENGTH];
         SHA256(reinterpret_cast<const unsigned char*>(input.c_str()), input.size(), hash);
 
-        std::stringstream hash_ss;
+        stringstream hash_ss;
         for (unsigned char i : hash) {
-            hash_ss << std::hex << (int)i;
+            hash_ss << hex << setw(2) << setfill('0') << (int)i;
         }
 
         return hash_ss.str();
     }
 };
 
-int main() {
-    Block genesisBlock(0, "Genesis Block", "0");
-    Block secondBlock(1, "Second Block", genesisBlock.hash);
 
-    std::cout << "Genesis Block Hash: " << genesisBlock.hash << std::endl;
-    std::cout << "Second Block Hash: " << secondBlock.hash << std::endl;
+class Blockchain {
+public:
+    Blockchain() {
+        chain.push_back(createGenesisBlock());
+    }
+
+    void addBlock(const string& data) {
+        const Block& prevBlock = chain.back();
+        chain.push_back(Block(prevBlock.index + 1, data, prevBlock.hash));
+    }
+
+    void printChain() const {
+        for (const auto& block : chain) {
+            cout << "Block " << block.index << ": " << block.hash << endl;
+        }
+    }
+
+private:
+    vector<Block> chain;
+    Block createGenesisBlock() {
+        return Block(0, "GenesisBlock", "0");
+    }
+};
+
+int main() {
+    Blockchain blockchain;
+
+    blockchain.addBlock("FirstBlock");
+    blockchain.addBlock("SecondBlock");
+
+    blockchain.printChain();
 
     return 0;
 }
